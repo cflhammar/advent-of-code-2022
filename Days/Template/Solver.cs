b425f40cf1d6b2e380c2151517fd938c330bf23b
Fredrik Hammar <fredrik.hammar@forefront.se>
2022-11-11 11:13:59 +0100

    Added some helpers from prev year

---
+++ Days/Template/Solver.cs
@@ -0,0 +1,20 @@
+using aoc_2022.Helpers;
+
+namespace aoc_2022.Days.Template;
+
+public class Solver : ISolver
+{
+    public string Date { get; } = "";
+    
+    public void Solve()
+    {
+        throw new NotImplementedException();
+    }
+
+    public dynamic ParseInput(string fileName)
+    {
+        var reader = new InputReader();
+
+        return reader;
+    }
+}
---
+++ Helpers/InputReader.cs
@@ -0,0 +1,63 @@
+namespace aoc_2022.Helpers;
+
+public class InputReader
+{
+    public string GetFileContent(string day, string fileName)
+    { 
+        var projectPath = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory())!.ToString())!.ToString());
+        var filePath = $"{projectPath}/Days/{day}/Data/{fileName}.txt";
+        var text = File.ReadAllText(filePath);
+
+        return text;
+    }
+    
+    public List<string> SplitByRow(string text)
+    {
+        var lines = text.Split("\n").ToList();
+
+        return lines;
+    }
+    
+    public List<string> SplitByEmptyRow(string text)
+    {
+        var lines = text.Split("\n\n").ToList();
+
+        return lines;
+    }
+    
+    public List<List<char>> SplitListOfStringToListListOfCharByNoDelimeter(List<string> input)
+    {
+        var output = input.Select(x => x.ToList()).ToList();
+        return output;
+    }
+    
+    public List<List<string>> SplitListOfStringToListListOfStringByDelimeter(List<string> input, string delimeter)
+    {
+        var output = new List<List<string>>();
+        
+        foreach (var line in input)
+        {
+            output.Add(line.Split(delimeter).ToList());
+        }
+        var output2 = input.Select(x => x.Split(delimeter).ToList()).ToList();
+        return output;
+    }
+    
+    public List<List<string>> SplitListOfStringToListListOfStringByRow(List<string> input)
+    {
+        var output = new List<List<string>>();
+        
+        foreach (var line in input)
+        {
+            output.Add(line.Split("\n").ToList());
+        }
+
+        return output;
+    }
+    
+    public List<string> SplitStringByDelimeterToListOfString(string s, string del)
+    {
+        return s.Split(del).ToList();
+    }
+
+}
